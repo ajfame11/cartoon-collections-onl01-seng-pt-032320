506e69c80ed089736bbad6433e1a32aef74e064a
aurel fontaine <ajfame11@gmail.com>
2020-04-01 07:53:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ def long_planeteer_calls(long_calls)
 end
 
 def find_the_cheese(cheese)
-  # the array below is here to help
+  cheese.find
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
